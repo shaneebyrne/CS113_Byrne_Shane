7a2dc50ca8d02639fd898544c868d768823d5c1d
shaneebyrne@gmail.com <_Qu3$t07_>
2017-01-24 19:06:53 -0800

    CS113 Shane Eire Byrne Lab 1

--- src/Lab1.java
+++ src/Lab1.java
@@ -7,7 +7,7 @@
  * Algorithm:
  *
  * Problem Statement:
- * Write a program that prints the numbers from 1 to 100,
+ * Write a program that prints the numbers from 2 to 100,
  * but for multiples of 3 print “Fizz” instead of the number and for multiples of 5 print “Buzz”.
  * For numbers that are multiples of both three and five print “FizzBuzz”.
  *
@@ -15,42 +15,22 @@
  *
  */
 public class Lab1 {
-    private final String MULTIPLE_OF_THREE = "Fizz";
-    private final String MULTIPLE_OF_FIVE = "Buzz";
-    private final String MULTIPLE_OF_THREE_AND_FIVE = "FizzBuzz";
-    private String aNumber;
+    private int aNumber;
 
-    public Lab1(String aNumber) {
+    public Lab1(int aNumber) {
         this.aNumber = aNumber;
     }
 
-    @Override
-    public String toString() {
-        return "Number:" + "\t" + aNumber;
-    }
-
-    public String getaNumber() {
+    public int getAnumber() {
         return aNumber;
     }
 
-    public void setaNumber(String aNumber) {
+    public void setAnumber(int aNumber) {
         this.aNumber = aNumber;
     }
 
     /**
-     * A Method that determines if a number is a multiple of 3
+     *  A method that checks if a number is a multiple of 3, 5, or both.
      */
-
-    public boolean isMultipleOfThree() {
-        return true;
-    }
-
-    /**
-     * A Method that determines if aNumber is a multiple of 5
-     */
-
-    public boolean isMultipleOfFive() {
-        return false;
-    }
-
 }
+
